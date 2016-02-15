f6b250d762470bfce5d6426df09e544462d62de6
Michael Totschnig <michael@totschnig.org>
2016-02-15 19:03:12 +0100

    lost during merge

--- myExpenses/src/main/java/org/totschnig/myexpenses/activity/MyExpenses.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/activity/MyExpenses.java
@@ -18,6 +18,7 @@ package org.totschnig.myexpenses.activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.database.Cursor;
