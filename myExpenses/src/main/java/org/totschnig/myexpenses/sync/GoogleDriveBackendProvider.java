9c0d41e94a9f3a4fb2e841bc5b91c10de1ac23e2
Michael Totschnig <michael@totschnig.org>
2017-01-12 12:21:35 +0100

    lock handling

--- myExpenses/src/main/java/org/totschnig/myexpenses/sync/GoogleDriveBackendProvider.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/sync/GoogleDriveBackendProvider.java
@@ -2,6 +2,7 @@ package org.totschnig.myexpenses.sync;
 
 import android.accounts.AccountManager;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 
 import com.annimon.stream.Collectors;
@@ -14,12 +15,15 @@ import com.google.android.gms.drive.DriveContents;
 import com.google.android.gms.drive.DriveFile;
 import com.google.android.gms.drive.DriveFolder;
 import com.google.android.gms.drive.DriveId;
+import com.google.android.gms.drive.DriveResource;
 import com.google.android.gms.drive.Metadata;
 import com.google.android.gms.drive.MetadataBuffer;
 import com.google.android.gms.drive.MetadataChangeSet;
 import com.google.android.gms.drive.metadata.CustomPropertyKey;
 
+import org.totschnig.myexpenses.BuildConfig;
 import org.totschnig.myexpenses.model.Account;
+import org.totschnig.myexpenses.model.Model;
 import org.totschnig.myexpenses.sync.json.AccountMetaData;
 import org.totschnig.myexpenses.sync.json.ChangeSet;
 
@@ -30,6 +34,9 @@ import java.util.List;
 import java.util.Map;
 
 public class GoogleDriveBackendProvider extends AbstractSyncBackendProvider {
+  public static final String KEY_LOCK_TOKEN = "lockToken";
+  public static final String KEY_OWNED_BY_US = "ownedByUs";
+  public static final String KEY_TIMESTAMP = "timestamp";
   private static final CustomPropertyKey ACCOUNT_METADATA_CURRENCY_KEY =
       new CustomPropertyKey("accountMetadataCurrency", CustomPropertyKey.PRIVATE);
   private static final CustomPropertyKey ACCOUNT_METADATA_COLOR_KEY =
@@ -42,12 +49,17 @@ public class GoogleDriveBackendProvider extends AbstractSyncBackendProvider {
       new CustomPropertyKey("accountMetadataDescription", CustomPropertyKey.PRIVATE);
   private static final CustomPropertyKey ACCOUNT_METADATA_TYPE_KEY =
       new CustomPropertyKey("accountMetadataType", CustomPropertyKey.PRIVATE);
+  private static final CustomPropertyKey LOCK_TOKEN_KEY =
+      new CustomPropertyKey(KEY_LOCK_TOKEN, CustomPropertyKey.PRIVATE);
+  private static final long LOCK_TIMEOUT = BuildConfig.DEBUG ? 60 * 1000 : 30 * 60 * 1000;
   private String folderId;
   private DriveFolder baseFolder, accountFolder;
 
   private GoogleApiClient googleApiClient;
+  private SharedPreferences sharedPreferences;
 
   GoogleDriveBackendProvider(Context context, android.accounts.Account account, AccountManager accountManager) {
+    sharedPreferences = context.getSharedPreferences("google_drive_backend_lock", 0);
     folderId = accountManager.getUserData(account, GenericAccountService.KEY_SYNC_PROVIDER_URL);
     googleApiClient = new GoogleApiClient.Builder(context)
         .addApi(Drive.API)
@@ -119,7 +131,39 @@ public class GoogleDriveBackendProvider extends AbstractSyncBackendProvider {
 
   @Override
   public boolean lock() {
-    return true;
+    Metadata metadata = accountFolder.getMetadata(googleApiClient).await().getMetadata();
+    if (metadata.getCustomProperties().containsKey(LOCK_TOKEN_KEY) &&
+        !shouldOverrideLock(metadata.getCustomProperties().get(LOCK_TOKEN_KEY))) {
+      googleApiClient.disconnect();
+      return false;
+    }
+    MetadataChangeSet.Builder changeSetBuilder = new MetadataChangeSet.Builder();
+
+    String lockToken = Model.generateUuid();
+    changeSetBuilder.setCustomProperty(LOCK_TOKEN_KEY, lockToken);
+    DriveResource.MetadataResult metadataResult =
+        accountFolder.updateMetadata(googleApiClient, changeSetBuilder.build()).await();
+    if (metadataResult.getStatus().isSuccess()) {
+      saveLockTokenToPreferences(lockToken, System.currentTimeMillis(), true);
+      return true;
+    }
+    return false;
+  }
+
+  private boolean shouldOverrideLock(String locktoken) {
+     long now = System.currentTimeMillis();
+    if (locktoken.equals(sharedPreferences.getString(KEY_LOCK_TOKEN, ""))) {
+      return sharedPreferences.getBoolean(KEY_OWNED_BY_US, false) ||
+          now - sharedPreferences.getLong(KEY_TIMESTAMP, 0) > LOCK_TIMEOUT;
+    } else {
+      saveLockTokenToPreferences(locktoken, now, false);
+      return false;
+    }
+  }
+
+  private void saveLockTokenToPreferences(String locktoken, long timestamp, boolean ownedByUs) {
+    sharedPreferences.edit().putString(KEY_LOCK_TOKEN, locktoken).putLong(KEY_TIMESTAMP, timestamp)
+        .putBoolean(KEY_OWNED_BY_US, ownedByUs).apply();
   }
 
   @Override
@@ -151,8 +195,13 @@ public class GoogleDriveBackendProvider extends AbstractSyncBackendProvider {
 
   @Override
   public boolean unlock() {
+    MetadataChangeSet.Builder changeSetBuilder = new MetadataChangeSet.Builder();
+
+    changeSetBuilder.deleteCustomProperty(LOCK_TOKEN_KEY);
+    boolean result = accountFolder.updateMetadata(googleApiClient, changeSetBuilder.build())
+        .await().getStatus().isSuccess();
     googleApiClient.disconnect();
-    return true;
+    return result;
   }
 
   @Override
