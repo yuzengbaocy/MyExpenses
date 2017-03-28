79e19c590a7cf23eb298d19dbe103fffc6b1c0c4
Michael Totschnig <michael@totschnig.org>
2017-03-28 21:34:37 +0200

    we want distribution be public to be able to use it in switch statements

--- myExpenses/src/main/java/org/totschnig/myexpenses/util/DistribHelper.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/util/DistribHelper.java
@@ -4,7 +4,7 @@ import org.totschnig.myexpenses.BuildConfig;
 
 public class DistribHelper {
 
-  private enum Distribution {
+  public enum Distribution {
     PLAY,
     AMAZON {
       @Override
@@ -54,14 +54,14 @@ public class DistribHelper {
     return !isBlackberry();
   }
 
-  private static Distribution getDistribution() {
+  public static Distribution getDistribution() {
     return Distribution.valueOf(BuildConfig.DISTRIBUTION);
   }
 
   public static String getDistributionAsString() {
     return getDistribution().toString();
   }
-  
+
   public static boolean isBlackberry() {
     return getDistribution().equals(Distribution.BLACKBERRY);
   }
