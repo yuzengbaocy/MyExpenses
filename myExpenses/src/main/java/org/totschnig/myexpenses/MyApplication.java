9ba5ef864f866d478c81823be357b0358fd6da14
Michael Totschnig <michael@totschnig.org>
2017-05-02 17:27:35 +0200

    removed analytics

--- myExpenses/src/main/java/org/totschnig/myexpenses/MyApplication.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/MyApplication.java
@@ -820,18 +820,5 @@ public class MyApplication extends MultiDexApplication implements
       StrictMode.setVmPolicy(builder.build());
     }
   }
-
-/*  *//**
-   * Gets the default {@link Tracker} for this {@link Application}.
-   * @return tracker
-   *//*
-  synchronized public Tracker getDefaultTracker() {
-    if (mTracker == null) {
-      GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
-      // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
-      mTracker = analytics.newTracker(R.xml.global_tracker);
-    }
-    return mTracker;
-  }*/
 }
 
