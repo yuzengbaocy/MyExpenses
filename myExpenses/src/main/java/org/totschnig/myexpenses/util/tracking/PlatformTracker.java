c183fe583488379b5748a16e6e983411e5b9ae41
Michael Totschnig <michael@totschnig.org>
2017-05-02 16:29:40 +0200

    firebase tracking

---
+++ myExpenses/src/main/java/org/totschnig/myexpenses/util/tracking/PlatformTracker.java
@@ -0,0 +1,27 @@
+package org.totschnig.myexpenses.util.tracking;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+import org.totschnig.myexpenses.util.Preconditions;
+
+public class PlatformTracker implements Tracker {
+  private FirebaseAnalytics firebaseAnalytics;
+  @Override
+  public void init(Context context) {
+    firebaseAnalytics = FirebaseAnalytics.getInstance(context);
+  }
+
+  @Override
+  public void logEvent(String eventName, Bundle params) {
+    Preconditions.checkNotNull(firebaseAnalytics);
+    firebaseAnalytics.logEvent(eventName, params);
+  }
+
+  @Override
+  public void setEnabled(boolean enabled) {
+    firebaseAnalytics.setAnalyticsCollectionEnabled(enabled);
+  }
+}
