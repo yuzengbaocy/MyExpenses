c975ee43aafa7118a34673bc94a69da4e64ab283
Michael Totschnig <michael@totschnig.org>
2017-08-04 12:02:16 +0200

    Adhandler remote configuration courtesy of Firebase

--- myExpenses/src/withAds/java/org/totschnig/myexpenses/util/ads/PlatformAdHandlerFactory.java
+++ myExpenses/src/withAds/java/org/totschnig/myexpenses/util/ads/PlatformAdHandlerFactory.java
@@ -1,10 +1,45 @@
 package org.totschnig.myexpenses.util.ads;
 
+import android.content.Context;
 import android.view.ViewGroup;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
+import org.totschnig.myexpenses.BuildConfig;
+import org.totschnig.myexpenses.R;
+
+import timber.log.Timber;
 
 public class PlatformAdHandlerFactory implements AdHandlerFactory {
   @Override
   public AdHandler create(ViewGroup adContainer) {
-    return new AmaAndAdmobAdHandler(adContainer);
+    Context context = adContainer.getContext();
+    if (AdHandler.isAdDisabled(context)) {
+      FirebaseAnalytics.getInstance(context).setUserProperty("AdHandler", "NoOp");
+      return new NoOpAdHandler(adContainer);
+    }
+    FirebaseRemoteConfig remoteConfig = FirebaseRemoteConfig.getInstance();
+    FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+        .setDeveloperModeEnabled(BuildConfig.DEBUG)
+        .build();
+    remoteConfig.setConfigSettings(configSettings);
+    remoteConfig.setDefaults(R.xml.remote_config_defaults);
+    remoteConfig.fetch().addOnCompleteListener(task -> {
+      remoteConfig.activateFetched();
+    });
+    String adHandler = remoteConfig.getString("ad_handler");
+    FirebaseAnalytics.getInstance(context).setUserProperty("AdHandler", adHandler);
+    switch (adHandler) {
+      case "PubNative": {
+        Timber.i("using PubNativeAdHandler");
+        return new PubNativeAdHandler(adContainer);
+      }
+      default: {
+        Timber.i("using AmaAndAdmobAdHandler");
+        return new AmaAndAdmobAdHandler(adContainer);
+      }
+    }
   }
 }
