36e4493eec2a87fcd8e07baf7ae77256ee31a89b
Michael Totschnig <michael@totschnig.org>
2017-08-12 02:45:04 +0200

    add google account email to backend in order to support backends from multiple Google accounts

--- myExpenses/src/withDrive/java/org.totschnig.myexpenses.sync/GoogleDriveBackendProviderFactory.java
+++ myExpenses/src/withDrive/java/org.totschnig.myexpenses.sync/GoogleDriveBackendProviderFactory.java
@@ -2,17 +2,42 @@ package org.totschnig.myexpenses.sync;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
+import com.annimon.stream.Collectors;
+import com.annimon.stream.Stream;
+import com.google.android.gms.common.AccountPicker;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi;
 
+import org.totschnig.myexpenses.MyApplication;
 import org.totschnig.myexpenses.R;
 import org.totschnig.myexpenses.activity.DriveSetupActivity;
+import org.totschnig.myexpenses.activity.ManageSyncBackends;
 import org.totschnig.myexpenses.activity.ProtectedFragmentActivity;
+import org.totschnig.myexpenses.preference.PrefKey;
+import org.totschnig.myexpenses.task.TaskExecutionFragment;
+import org.totschnig.myexpenses.util.NotificationBuilderWrapper;
+import org.totschnig.myexpenses.util.Result;
+import org.totschnig.myexpenses.util.Utils;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Locale;
+
+import static org.totschnig.myexpenses.sync.GoogleDriveBackendProvider.KEY_GOOGLE_ACCOUNT_EMAIL;
 
 public class GoogleDriveBackendProviderFactory extends SyncBackendProviderFactory {
   public static final String LABEL = "Drive";
@@ -44,5 +69,103 @@ public class GoogleDriveBackendProviderFactory extends SyncBackendProviderFactor
   @Override
   public int getId() {
     return R.id.SYNC_BACKEND_DRIVE;
+  }
+
+  @Override
+  public Intent getRepairIntent(Activity activity) {
+    AccountManager accountManager = AccountManager.get(activity);
+    if (GenericAccountService.getAccountsAsStream(activity)
+        .anyMatch(account -> account.name.startsWith(LABEL) &&
+            accountManager.getUserData(account, GoogleDriveBackendProvider.KEY_GOOGLE_ACCOUNT_EMAIL) == null)) {
+      return AccountPicker.newChooseAccountIntent(null,
+          null, new String[]{"com.google"}, true, activity. getString(R.string.drive_backend_upgrade), null, null, null);
+    }
+    return null;
+  }
+
+  @Override
+  public boolean startRepairTask(ManageSyncBackends activity, Intent data) {
+    activity.startTaskExecution(
+        TaskExecutionFragment.TASK_REPAIR_SYNC_BACKEND, new String[]{LABEL},
+        data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME),
+        R.string.progress_dialog_checking_sync_backend);
+    return true;
+  }
+
+  @Override
+  public Result handleRepairTask(Serializable mExtra) {
+    String accountName = (String) mExtra;
+    Context application = MyApplication.getInstance();
+    final AccountManager accountManager = AccountManager.get(application);
+    GoogleApiClient googleApiClient = new GoogleApiClient.Builder(application)
+        .addApi(Drive.API)
+        .addScope(Drive.SCOPE_FILE)
+        .setAccountName(accountName)
+        .build();
+    ConnectionResult connectionResult = googleApiClient.blockingConnect();
+    if (!connectionResult.isSuccess()) {
+      Log.e("DEBUG", connectionResult.getErrorMessage());
+      return new Result(false, R.string.sync_io_error_cannot_connect, connectionResult.getResolution());
+    }
+    List<Account> legacyDriveAccounts = GenericAccountService.getAccountsAsStream(application)
+        .filter(account -> account.name.startsWith(GoogleDriveBackendProviderFactory.LABEL) &&
+            accountManager.getUserData(account, KEY_GOOGLE_ACCOUNT_EMAIL) == null)
+        .collect(Collectors.toList());
+    int allCount = legacyDriveAccounts.size();
+    int fixCount = Stream.of(legacyDriveAccounts)
+        .mapToInt(account -> {
+          DriveApi.DriveIdResult driveIdResult = Drive.DriveApi.fetchDriveId(googleApiClient,
+              accountManager.getUserData(account, GenericAccountService.KEY_SYNC_PROVIDER_URL)).await();
+          if (driveIdResult.getStatus().isSuccess()) {
+            accountManager.setUserData(account, KEY_GOOGLE_ACCOUNT_EMAIL,
+                accountName);
+            Log.i("DEBUG", String.format("Linked %s with %s", account.name, accountName));
+            return 1;
+          } else {
+            return 0;
+          }
+        }).sum();
+    boolean success = allCount == fixCount;
+    String result = success ? "Success" : "Failure";
+    googleApiClient.disconnect();
+    return new Result(success, String.format(Locale.ROOT, "%s: %d/%d", result, fixCount, allCount));
+  }
+
+  @Override
+  public void init() {
+    if (PrefKey.CURRENT_VERSION.getInt(-1) < 288) {
+      migrateDriveAccountsAttempt();
+    }
+  }
+
+  private void migrateDriveAccountsAttempt() {
+    Context application = MyApplication.getInstance();
+    List<android.accounts.Account> driveAccounts = GenericAccountService.getAccountsAsStream(application)
+        .filter(account -> account.name.startsWith(GoogleDriveBackendProviderFactory.LABEL))
+        .collect(Collectors.toList());
+    if (driveAccounts.size() > 0) {
+      if (Utils.hasApiLevel(Build.VERSION_CODES.M)) { //does not work on new permission model, since we do not hold GET_ACCOUNTS permission
+        final AccountManager accountManager = AccountManager.get(application);
+        android.accounts.Account googleAccounts[] = accountManager.getAccountsByType("com.google");
+        if (googleAccounts.length == 1) {
+          Stream.of(driveAccounts).forEach(account -> {
+            accountManager.setUserData(account, KEY_GOOGLE_ACCOUNT_EMAIL,
+                googleAccounts[0].name);
+            Log.i("DEBUG", String.format("Linked %s with %s", account.name, googleAccounts[0].name));
+          });
+          return;
+        }
+      }
+      NotificationBuilderWrapper builder = NotificationBuilderWrapper.defaultBigTextStyleBuilder(
+          application, application.getString(R.string.important_upgrade_information_heading),
+          application.getString(R.string.drive_backend_upgrade))
+          .setContentIntent(PendingIntent.getActivity(
+              application, 0, new Intent(application, ManageSyncBackends.class),
+              PendingIntent.FLAG_CANCEL_CURRENT));
+      Notification notification = builder.build();
+      notification.flags = Notification.FLAG_AUTO_CANCEL;
+      ((NotificationManager) application.getSystemService(Context.NOTIFICATION_SERVICE)).notify(
+          "SYNC", 0, notification);
+    }
   }
 }
