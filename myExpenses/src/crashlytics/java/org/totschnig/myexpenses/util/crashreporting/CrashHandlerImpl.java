c205a63f8712ca4da4795afaa6e2f771b7b6420d
Michael Totschnig <michael@totschnig.org>
2018-03-19 09:01:10 +0100

    minor adjustment to crash reporting

--- myExpenses/src/crashlytics/java/org/totschnig/myexpenses/util/crashreporting/CrashHandlerImpl.java
+++ myExpenses/src/crashlytics/java/org/totschnig/myexpenses/util/crashreporting/CrashHandlerImpl.java
@@ -1,7 +1,6 @@
 package org.totschnig.myexpenses.util.crashreporting;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
@@ -35,10 +34,10 @@ public class CrashHandlerImpl extends CrashHandler {
 
   private static class CrashReportingTree extends Timber.Tree {
     @Override
-    protected void log(int priority, String tag, @NonNull String message, Throwable t) {
+    protected void log(int priority, String tag, String message, Throwable t) {
       if (priority == Log.ERROR && t != null) {
         Crashlytics.logException(t);
-      } else {
+      } else if (message != null) {
         Crashlytics.log(message);
       }
     }
