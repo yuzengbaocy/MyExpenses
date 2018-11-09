d5d6f2a1ef2a6af057b0ea3247afb7cdc0f5616a
Michael Totschnig <michael@totschnig.org>
2018-11-09 11:55:42 +0100

    on priority error, we logException with message wrapped

--- myExpenses/src/crashlytics/java/org/totschnig/myexpenses/util/crashreporting/CrashHandlerImpl.java
+++ myExpenses/src/crashlytics/java/org/totschnig/myexpenses/util/crashreporting/CrashHandlerImpl.java
@@ -5,6 +5,8 @@ import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.totschnig.myexpenses.MyApplication;
 import org.totschnig.myexpenses.preference.PrefKey;
 
@@ -39,10 +41,10 @@ public class CrashHandlerImpl extends CrashHandler {
 
   private static class CrashReportingTree extends Timber.Tree {
     @Override
-    protected void log(int priority, String tag, String message, Throwable t) {
-      if (priority == Log.ERROR && t != null) {
-        Crashlytics.logException(t);
-      } else if (message != null) {
+    protected void log(int priority, @Nullable String tag, @NotNull String message, @Nullable Throwable t) {
+      if (priority == Log.ERROR) {
+        Crashlytics.logException(t == null ? new Exception(message) : t);
+      } else {
         Crashlytics.log(message);
       }
     }
