f33a2fe81603108ff8d9e3b6b2aa0d8246c9ee22
Michael Totschnig <michael@totschnig.org>
2018-12-06 15:27:37 +0100

    sharded sync

--- myExpenses/src/withDrive/java/org.totschnig.myexpenses.sync/GoogleDriveBackendProvider.java
+++ myExpenses/src/withDrive/java/org.totschnig.myexpenses.sync/GoogleDriveBackendProvider.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 
+import com.annimon.stream.Collectors;
 import com.annimon.stream.Exceptional;
 import com.annimon.stream.Optional;
 import com.annimon.stream.Stream;
@@ -45,11 +46,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import timber.log.Timber;
 
 public class GoogleDriveBackendProvider extends AbstractSyncBackendProvider {
   private static final String KEY_LAST_FAILED_SYNC = "lastFailedSync";
@@ -104,7 +104,7 @@ public class GoogleDriveBackendProvider extends AbstractSyncBackendProvider {
     long currentBackOff = sharedPreferences.getLong(KEY_SYNC_BACKOFF, 0);
     long now = System.currentTimeMillis();
     if (lastFailedSync != 0 && lastFailedSync + currentBackOff > now) {
-      Timber.e("Not syncing, waiting for another %d milliseconds", lastFailedSync + currentBackOff - now);
+      SyncAdapter.log().e("Not syncing, waiting for another %d milliseconds", lastFailedSync + currentBackOff - now);
       return SUCCESS;
     }
     if (googleApiClient.isConnected()) {
@@ -134,13 +134,13 @@ public class GoogleDriveBackendProvider extends AbstractSyncBackendProvider {
   private Exceptional<Void> setUpInternal(long now) {
     Status status = Drive.DriveApi.requestSync(googleApiClient).await();
     if (!status.isSuccess()) {
-      Timber.e("Sync failed with code %d", status.getStatusCode());
+      SyncAdapter.log().e("Sync failed with code %d", status.getStatusCode());
       long newBackOff = Math.min(sharedPreferences.getLong(KEY_SYNC_BACKOFF, 5000) * 2, 3600000);
-      Timber.e("Backing off for %d milliseconds ", newBackOff);
+      SyncAdapter.log().e("Backing off for %d milliseconds ", newBackOff);
       sharedPreferences.edit().putLong(KEY_LAST_FAILED_SYNC, now).putLong(KEY_SYNC_BACKOFF, newBackOff).commit();
       return SUCCESS;
     } else {
-      Timber.i("Sync succeeded");
+      SyncAdapter.log().i("Sync succeeded");
       sharedPreferences.edit().remove(KEY_LAST_FAILED_SYNC).remove(KEY_SYNC_BACKOFF).apply();
       return SUCCESS;
     }
@@ -233,25 +233,63 @@ public class GoogleDriveBackendProvider extends AbstractSyncBackendProvider {
   }
 
   @Override
-  protected long getLastSequence(long start) throws IOException {
-    DriveApi.MetadataBufferResult metadataBufferResult = accountFolder.listChildren(googleApiClient).await();
-    if (!metadataBufferResult.getStatus().isSuccess()) {
+  protected SequenceNumber getLastSequence(SequenceNumber start) throws IOException {
+    final Comparator<Metadata> resourceComparator = (o1, o2) -> Utils.compare(getSequenceFromFileName(o1.getTitle()), getSequenceFromFileName(o2.getTitle()));
+
+    DriveApi.MetadataBufferResult mainResult = accountFolder.listChildren(googleApiClient).await();
+    if (!mainResult.getStatus().isSuccess()) {
       throw new IOException("Error while trying to get last sequence");
     }
-    MetadataBuffer metadataBuffer = metadataBufferResult.getMetadataBuffer();
-    Long result = Stream.of(metadataBuffer)
-        .filter(metadata -> isNewerJsonFile(start, metadata.getTitle()))
-        .map(metadata -> getSequenceFromFileName(metadata.getTitle()))
-        .max(Utils::compare)
-        .orElse(start);
+    final MetadataBuffer metadataBuffer = mainResult.getMetadataBuffer();
+    Optional<Metadata> lastShardOptional =
+        Stream.of(metadataBuffer)
+            .filter(metadata -> metadata.isFolder() && isAtLeastShardDir(start.shard, metadata.getTitle()))
+            .max(resourceComparator);
+
+    MetadataBuffer lastShard;
+    int lastShardInt, reference;
+    if (lastShardOptional.isPresent()) {
+      lastShard = lastShardOptional.get().getDriveId().asDriveFolder().listChildren(googleApiClient).await().getMetadataBuffer();
+      lastShardInt = getSequenceFromFileName(lastShardOptional.get().getTitle());
+      reference = lastShardInt == start.shard ? start.number : 0;
+    } else {
+      if (start.shard > 0) return start;
+      //mainResult can not be reused
+      lastShard = accountFolder.listChildren(googleApiClient).await().getMetadataBuffer();
+      lastShardInt = 0;
+      reference = start.number;
+    }
     metadataBuffer.release();
+    SequenceNumber result = Stream.of(lastShard)
+        .filter(metadata -> isNewerJsonFile(reference, metadata.getTitle()))
+        .max(resourceComparator)
+        .map(metadata -> new SequenceNumber(lastShardInt, getSequenceFromFileName(metadata.getTitle())))
+        .orElse(start);
+    lastShard.release();
     return result;
   }
 
   @Override
-  void saveFileContents(String fileName, String fileContents, String mimeType) throws IOException {
+  void saveFileContents(String folder, String fileName, String fileContents, String mimeType) throws IOException {
+    DriveFolder driveFolder;
+    if (folder == null) {
+      driveFolder = accountFolder;
+    } else {
+      final Optional<DriveFolder> driveFolderOptional = getShardFolder(folder);
+      if (driveFolderOptional.isPresent()) {
+        driveFolder = driveFolderOptional.get();
+      } else {
+        MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
+            .setTitle(folder).build();
+        DriveFolder.DriveFolderResult driveFolderResult = accountFolder.createFolder(googleApiClient, changeSet).await();
+        if (!driveFolderResult.getStatus().isSuccess()) {
+          throw new IOException("Unable to create folder");
+        }
+        driveFolder = driveFolderResult.getDriveFolder();
+      }
+    }
     ByteArrayInputStream contents = new ByteArrayInputStream(fileContents.getBytes());
-    saveInputStream(fileName, contents, mimeType, accountFolder);
+    saveInputStream(fileName, contents, mimeType, driveFolder);
     contents.close();
   }
 
@@ -293,7 +331,7 @@ public class GoogleDriveBackendProvider extends AbstractSyncBackendProvider {
     if (!driveFileResult.getStatus().isSuccess()) {
       throw new IOException("Error while trying to create file");
     }
-    Timber.tag(SyncAdapter.TAG).i(driveFileResult.getStatus().toString());
+    SyncAdapter.log().i(driveFileResult.getStatus().toString());
   }
 
   @Override
@@ -316,35 +354,74 @@ public class GoogleDriveBackendProvider extends AbstractSyncBackendProvider {
   @Override
   public
   @NonNull
-  ChangeSet getChangeSetSince(long sequenceNumber, Context context) throws IOException {
-    DriveApi.MetadataBufferResult metadataBufferResult = accountFolder.listChildren(googleApiClient).await();
+  ChangeSet getChangeSetSince(SequenceNumber sequenceNumber, Context context) throws IOException {
+    DriveFolder shardFolder;
+    if (sequenceNumber.shard == 0) {
+      shardFolder = accountFolder;
+    } else {
+      shardFolder = getShardFolder("_" + sequenceNumber.shard).orElseThrow(() -> new IOException("shard folder not found"));
+    }
+    DriveApi.MetadataBufferResult metadataBufferResult = shardFolder.listChildren(googleApiClient).await();
     if (!metadataBufferResult.getStatus().isSuccess()) {
       throw new IOException("Error while trying to get change set");
     }
     MetadataBuffer metadataBuffer = metadataBufferResult.getMetadataBuffer();
-    ChangeSet result = merge(Stream.of(metadataBuffer)
-        .filter(metadata -> isNewerJsonFile(sequenceNumber, metadata.getTitle()))
-        .map(this::getChangeSetFromMetadata))
-        .orElse(ChangeSet.empty(sequenceNumber));
+    final List<ChangeSet> entries = Stream.of(metadataBuffer)
+        .filter(metadata -> isNewerJsonFile(sequenceNumber.number, metadata.getTitle()))
+        .map(metadata -> getChangeSetFromMetadata(sequenceNumber.shard, metadata))
+        .collect(Collectors.toList());
+    metadataBuffer.release();
+    int nextShard = sequenceNumber.shard + 1;
+    while (true) {
+      Optional<DriveFolder> nextShardFolder = getShardFolder("_" + nextShard);
+      if (nextShardFolder.isPresent()) {
+        metadataBufferResult = nextShardFolder.get().listChildren(googleApiClient).await();
+        if (!metadataBufferResult.getStatus().isSuccess()) {
+          throw new IOException("Error while trying to get change set");
+        }
+        metadataBuffer = metadataBufferResult.getMetadataBuffer();
+        Stream.of(metadataBuffer)
+            .filter(metadata -> isNewerJsonFile(0, metadata.getTitle()))
+            .map(metadata -> getChangeSetFromMetadata(nextShard, metadata))
+            .forEach(entries::add);
+        metadataBuffer.release();
+      } else {
+        break;
+      }
+    }
+
+    return merge(Stream.of(entries)).orElse(ChangeSet.empty(sequenceNumber));
+  }
+
+  private Optional<DriveFolder> getShardFolder(String shard) throws IOException {
+    DriveApi.MetadataBufferResult metadataBufferResult = accountFolder.listChildren(googleApiClient).await();
+    if (!metadataBufferResult.getStatus().isSuccess()) {
+      throw new IOException("Unable to list children of account folder");
+    }
+    MetadataBuffer metadataBuffer = metadataBufferResult.getMetadataBuffer();
+    Optional<DriveFolder> result = Stream.of(metadataBuffer)
+        .filter(metadata -> metadata.getTitle().equals(shard))
+        .findFirst()
+        .map(metadata -> metadata.getDriveId().asDriveFolder());
     metadataBuffer.release();
     return result;
   }
 
-  private ChangeSet getChangeSetFromMetadata(Metadata metadata) {
+  private ChangeSet getChangeSetFromMetadata(int shard, Metadata metadata) {
     DriveId driveId = metadata.getDriveId();
     DriveApi.DriveContentsResult driveContentsResult =
         driveId.asDriveFile().open(googleApiClient, DriveFile.MODE_READ_ONLY, null).await();
     if (!driveContentsResult.getStatus().isSuccess()) {
       SyncAdapter.log().e("Unable to open %s", driveId.getResourceId());
-      return ChangeSet.failed;
+      return null;
     }
     DriveContents driveContents = driveContentsResult.getDriveContents();
     try {
-      return getChangeSetFromInputStream(getSequenceFromFileName(metadata.getTitle()),
+      return getChangeSetFromInputStream(new SequenceNumber(shard, getSequenceFromFileName(metadata.getTitle())),
           driveContents.getInputStream());
     } catch (IOException e) {
       SyncAdapter.log().w(e);
-      return ChangeSet.failed;
+      return null;
     } finally {
       driveContents.discard(googleApiClient);
     }
