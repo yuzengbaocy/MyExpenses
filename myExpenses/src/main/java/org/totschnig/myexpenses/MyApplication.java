82dce74c04cf53b2cb2e7904097496ae3c11a11b
Michael Totschnig <michael@totschnig.org>
2019-08-30 07:54:23 +0200

    version upgrades, use mockito-android instead of dexmaker use strictmode without death penalty prevent leaked cursor

--- myExpenses/src/main/java/org/totschnig/myexpenses/MyApplication.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/MyApplication.java
@@ -802,7 +802,6 @@ public class MyApplication extends MultiDexApplication implements
     StrictMode.setThreadPolicy(threadPolicyBuilder.build());
     StrictMode.VmPolicy.Builder vmPolicyBuilder = new StrictMode.VmPolicy.Builder()
         .detectAll()
-        .penaltyDeath()
         .penaltyLog();
     StrictMode.setVmPolicy(vmPolicyBuilder.build());
   }
