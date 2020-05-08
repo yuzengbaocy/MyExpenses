af63534f2f5364782e0a2dd9d17cc9e014b7914a
Michael Totschnig <michael@totschnig.org>
2020-05-08 02:20:01 +0200

    refactored

--- myExpenses/src/main/java/org/totschnig/myexpenses/di/UiModule.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/di/UiModule.kt
@@ -1,5 +1,8 @@
 package org.totschnig.myexpenses.di
 
+import android.app.Activity
+import android.app.Application
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 import org.totschnig.myexpenses.MyApplication
@@ -9,7 +12,6 @@ import org.totschnig.myexpenses.preference.PrefHandler
 import org.totschnig.myexpenses.util.ads.AdHandlerFactory
 import org.totschnig.myexpenses.util.ads.PlatformAdHandlerFactory
 import org.totschnig.myexpenses.util.bundle.LocaleManager
-import java.util.*
 import javax.inject.Named
 import javax.inject.Singleton
 
@@ -22,15 +24,34 @@ class UiModule {
 
     @Provides
     @Singleton
-    fun provideAdHandlerFactory(application: MyApplication?, prefHandler: PrefHandler?, @Named(AppComponent.USER_COUNTRY) userCountry: String?): AdHandlerFactory =
+    fun provideAdHandlerFactory(application: MyApplication, prefHandler: PrefHandler, @Named(AppComponent.USER_COUNTRY) userCountry: String): AdHandlerFactory =
             PlatformAdHandlerFactory(application, prefHandler, userCountry)
 
     @Provides
     @Singleton
-    fun provideLanguageManager(): LocaleManager {
-        return object : LocaleManager {
-            override fun requestLocale(locale: Locale, onAvailable: () -> Unit) {
-                onAvailable()
+    fun provideLanguageManager(): LocaleManager = try {
+        Class.forName("org.totschnig.myexpenses.util.bundle.PlatformLocaleManager").newInstance() as LocaleManager
+    } catch (e: Exception) {
+        object : LocaleManager {
+            var callback: (() -> Unit)? = null
+            override fun initApplication(application: Application) {
+                //noop
+            }
+
+            override fun initActivity(activity: Activity) {
+                //noop
+            }
+
+            override fun requestLocale(context: Context) {
+                callback?.invoke()
+            }
+
+            override fun onResume(onAvailable: () -> Unit) {
+               this.callback = onAvailable
+            }
+
+            override fun onPause() {
+                this.callback = null
             }
         }
     }
