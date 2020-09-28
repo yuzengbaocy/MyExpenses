97e06fbdc04cc309deeca9e57dafb31180de58c9
Michael Totschnig <michael@totschnig.org>
2020-09-28 15:34:14 +0200

    migrate crashlytics

--- myExpenses/src/main/java/org/totschnig/myexpenses/util/crashreporting/CrashlyticsHandler.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/util/crashreporting/CrashlyticsHandler.java
@@ -1,52 +1,30 @@
 package org.totschnig.myexpenses.util.crashreporting;
 
 import android.content.Context;
-import android.os.Handler;
 import android.util.Log;
 
-import com.crashlytics.android.Crashlytics;
+import com.google.firebase.crashlytics.FirebaseCrashlytics;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.totschnig.myexpenses.MyApplication;
 import org.totschnig.myexpenses.preference.PrefKey;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-
-import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
 
 public class CrashlyticsHandler extends CrashHandler {
   private CrashReportingTree crashReportingTree;
-  private Map<String, String> delayedKeys = new HashMap<>();
 
   @Override
   public void onAttachBaseContext(MyApplication application) {
-
   }
 
   @Override
   void setupLoggingDo(Context context) {
-    Fabric.with(context, new Crashlytics());
     setKeys(context);
     if (crashReportingTree == null) {
       crashReportingTree = new CrashReportingTree();
       Timber.plant(crashReportingTree);
-    }
-    final Handler handler = new Handler();
-    handler.postDelayed(this::setDelayedKeys, 5000);
-  }
-
-  private void setDelayedKeys() {
-    if (Fabric.isInitialized()) {
-      Iterator<Map.Entry<String, String>> iter = delayedKeys.entrySet().iterator();
-      while (iter.hasNext()) {
-        Map.Entry<String, String> entry = iter.next();
-        Crashlytics.setString(entry.getKey(), entry.getValue());
-        iter.remove();
-      }
     }
   }
 
@@ -59,20 +37,16 @@ public class CrashlyticsHandler extends CrashHandler {
 
   @Override
   public void putCustomData(String key, String value) {
-    if (Fabric.isInitialized()) {
-      Crashlytics.setString(key, value);
-    } else {
-      delayedKeys.put(key, value);
-    }
+    FirebaseCrashlytics.getInstance().setCustomKey(key, value);
   }
 
   private static class CrashReportingTree extends Timber.Tree {
     @Override
     protected void log(int priority, @Nullable String tag, @NotNull String message, @Nullable Throwable t) {
       if (priority == Log.ERROR) {
-        Crashlytics.logException(t == null ? new Exception(message) : t);
+        FirebaseCrashlytics.getInstance().recordException(t == null ? new Exception(message) : t);
       } else {
-        Crashlytics.log(message);
+        FirebaseCrashlytics.getInstance().log(message);
       }
     }
 
