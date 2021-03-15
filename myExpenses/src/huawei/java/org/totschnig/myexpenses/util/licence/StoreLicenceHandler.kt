fc289e38bc0e1566eaeb035bb3097e4c3b47a416
Michael Totschnig <michael@totschnig.org>
2021-03-15 18:58:38 +0100

    bring huawei inline with refactoring

--- myExpenses/src/huawei/java/org/totschnig/myexpenses/util/licence/StoreLicenceHandler.kt
+++ myExpenses/src/huawei/java/org/totschnig/myexpenses/util/licence/StoreLicenceHandler.kt
@@ -3,14 +3,17 @@ package org.totschnig.myexpenses.util.licence
 import android.content.Context
 import com.google.android.vending.licensing.PreferenceObfuscator
 import org.totschnig.myexpenses.MyApplication
+import org.totschnig.myexpenses.preference.PrefHandler
 import org.totschnig.myexpenses.util.crashreporting.CrashHandler
 import java.util.*
 
-class StoreLicenceHandler(application: MyApplication, preferenceObfuscator: PreferenceObfuscator, crashHandler: CrashHandler):
-        PlayStoreLicenceHandler(application, preferenceObfuscator, crashHandler) {
-    override fun needsKeyEntry() = true
+class StoreLicenceHandler(context: MyApplication, preferenceObfuscator: PreferenceObfuscator, crashHandler: CrashHandler, prefHandler: PrefHandler) :
+        PlayStoreLicenceHandler(context, preferenceObfuscator, crashHandler, prefHandler) {
+    override val needsKeyEntry: Boolean
+        get() = true
 
-    override fun doesUseIAP() = false
+    override val doesUseIAP: Boolean
+        get() = false
 
     override fun getFormattedPrice(aPackage: Package) = getFormattedPriceWithExtra(aPackage, false)
 
