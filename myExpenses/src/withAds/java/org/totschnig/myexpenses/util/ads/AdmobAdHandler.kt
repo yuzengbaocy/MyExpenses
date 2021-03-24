57de87b29bb631441f406d319a5204462a23e64a
Michael Totschnig <michael@totschnig.org>
2021-03-24 15:11:24 +0100

    2 kotlin reactivate firebase remote configuration show default

---
+++ myExpenses/src/withAds/java/org/totschnig/myexpenses/util/ads/AdmobAdHandler.kt
@@ -0,0 +1,146 @@
+package org.totschnig.myexpenses.util.ads
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.View
+import android.view.ViewGroup
+import com.google.ads.mediation.admob.AdMobAdapter
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import org.totschnig.myexpenses.BuildConfig
+import org.totschnig.myexpenses.R
+import org.totschnig.myexpenses.activity.BaseActivity
+import org.totschnig.myexpenses.preference.PrefKey
+
+internal class AdmobAdHandler(factory: AdHandlerFactory, adContainer: ViewGroup, baseActivity: BaseActivity, private val bannerUnitId: Int, private val interstitialUnitId: Int, private val smartSegmentationInterstitialUnitId: Int) : BaseAdHandler(factory, adContainer, baseActivity) {
+    private var admobView: AdView? = null
+    private var admobInterstitialAd: InterstitialAd? = null
+    private var mAdMobBannerShown = false
+    private var mInterstitialShown = false
+    private var interstitialCounter = 0
+    public override fun initInternal() {
+        MobileAds.initialize(activity)
+        /*    List<String> testDeviceIds = Collections.singletonList("837D45A603F3C5E72CECC450C2CE4A63");
+    RequestConfiguration configuration =
+        new RequestConfiguration.Builder().setTestDeviceIds(testDeviceIds).build();
+    MobileAds.setRequestConfiguration(configuration);*/
+    }
+
+    override fun startBannerInternal() {
+        showBannerAdmob()
+    }
+
+    private fun showBannerAdmob() {
+        if (bannerUnitId == 0) {
+            hide()
+            return
+        }
+        admobView = AdView(activity).apply {
+            adSize = calculateAdSize
+            adUnitId = context.getString(if (isTest) R.string.admob_unitid_test_banner else bannerUnitId)
+            adContainer.addView(this)
+            adListener = object : AdListener() {
+                override fun onAdLoaded() {
+                    trackBannerLoaded(PROVIDER_ADMOB)
+                    mAdMobBannerShown = true
+                    visibility = View.VISIBLE
+                }
+
+                override fun onAdFailedToLoad(i: Int) {
+                    trackBannerFailed(PROVIDER_ADMOB, i.toString())
+                    hide()
+                }
+            }
+            loadAd(buildAdmobRequest())
+        }
+        trackBannerRequest(PROVIDER_ADMOB)
+    }
+
+    private val calculateAdSize: AdSize
+        get() {
+            val display = (activity as Activity).windowManager.defaultDisplay
+            val outMetrics = DisplayMetrics()
+            display.getMetrics(outMetrics)
+            val density = outMetrics.density
+            val adWidth = (adContainer.width / density).toInt()
+            return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(activity, adWidth)
+        }
+
+    private fun buildAdmobRequest(): AdRequest {
+        val builder = AdRequest.Builder()
+        if (!prefHandler.getBoolean(PrefKey.PERSONALIZED_AD_CONSENT, true)) {
+            val extras = Bundle()
+            extras.putString("npa", "1")
+            builder.addNetworkExtrasBundle(AdMobAdapter::class.java, extras)
+        }
+        return builder.build()
+    }
+
+    private val isTest: Boolean
+        get() = BuildConfig.DEBUG
+
+    override fun requestNewInterstitialDo() {
+        mInterstitialShown = false
+        interstitialCounter++
+        val adUnitId = activity.getString(if (isTest) R.string.admob_unitid_test_interstitial else
+            smartSegmentationInterstitialUnitId.takeIf { it != 0 && (interstitialCounter % 5 != 0)  } ?: interstitialUnitId)
+        trackInterstitialRequest(PROVIDER_ADMOB)
+        InterstitialAd.load(activity, adUnitId, buildAdmobRequest(), object: InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(loadAdError: LoadAdError) {
+                trackInterstitialFailed(PROVIDER_ADMOB, loadAdError.toString())
+                onInterstitialFailed()
+            }
+
+            override fun onAdLoaded(ad: InterstitialAd) {
+                trackInterstitialLoaded(PROVIDER_ADMOB)
+                admobInterstitialAd = ad
+            }
+        })
+    }
+
+    override fun maybeShowInterstitialDo() = if (mInterstitialShown) false else
+        admobInterstitialAd?.let {
+            trackInterstitialShown(PROVIDER_ADMOB)
+            MobileAds.setAppVolume(0f)
+            it.show(activity)
+            mInterstitialShown = true
+            true
+        } ?: false
+
+    override fun onResume() {
+        if (mAdMobBannerShown) {
+            //activity might have been resumed after user has bought contrib key
+            if (shouldHideAd()) {
+                admobView?.destroy()
+                hide()
+                mAdMobBannerShown = false
+            } else {
+                admobView?.resume()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        if (mAdMobBannerShown) {
+            admobView?.destroy()
+            mAdMobBannerShown = false
+        }
+    }
+
+    override fun onPause() {
+        if (mAdMobBannerShown) {
+            admobView?.pause()
+        }
+    }
+
+    companion object {
+        private const val PROVIDER_ADMOB = "Admob"
+    }
+}
---
+++ myExpenses/src/withAds/java/org/totschnig/myexpenses/util/ads/PlatformAdHandlerFactory.kt
@@ -0,0 +1,46 @@
+package org.totschnig.myexpenses.util.ads
+
+import android.content.Context
+import android.view.ViewGroup
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
+import org.totschnig.myexpenses.BuildConfig
+import org.totschnig.myexpenses.R
+import org.totschnig.myexpenses.activity.BaseActivity
+import org.totschnig.myexpenses.preference.PrefHandler
+import org.totschnig.myexpenses.util.crashreporting.CrashHandler
+import org.totschnig.myexpenses.util.licence.LicenceHandler
+
+class PlatformAdHandlerFactory(context: Context, prefHandler: PrefHandler, userCountry: String, licenceHandler: LicenceHandler) : DefaultAdHandlerFactory(context, prefHandler, userCountry, licenceHandler) {
+    override fun create(adContainer: ViewGroup, baseActivity: BaseActivity): AdHandler {
+        val adHandler = if (isAdDisabled) "NoOp" else {
+            val remoteConfig = FirebaseRemoteConfig.getInstance()
+            if (BuildConfig.DEBUG) {
+                val configSettings = FirebaseRemoteConfigSettings.Builder()
+                        .setMinimumFetchIntervalInSeconds(0)
+                        .build()
+                remoteConfig.setConfigSettingsAsync(configSettings)
+            }
+            remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults)
+            remoteConfig.fetchAndActivate().addOnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    CrashHandler.report("Firebase Remote Config Fetch failed")
+                }
+            }
+            remoteConfig.getString("ad_handling_waterfall").split(":".toRegex()).getOrNull(0) ?: "AdMob"
+        }
+        FirebaseAnalytics.getInstance(context).setUserProperty("AdHandler", adHandler)
+        return instantiate(adHandler, adContainer, baseActivity)
+    }
+
+    private fun instantiate(handler: String, adContainer: ViewGroup, baseActivity: BaseActivity): AdHandler {
+        return when (handler) {
+            "Custom" -> AdmobAdHandler(this, adContainer, baseActivity,
+                    R.string.admob_unitid_custom_banner, R.string.admob_unitid_custom_interstitial, 0)
+            "AdMob" -> AdmobAdHandler(this, adContainer, baseActivity,
+                    R.string.admob_unitid_mainscreen, R.string.admob_unitid_interstitial, R.string.admob_unitid_interstitial_smart_segmentation)
+            else -> NoOpAdHandler
+        }
+    }
+}
